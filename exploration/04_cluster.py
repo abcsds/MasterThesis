e5671ff8739f6f3ad9dc9d423d54d62ca0eacb80
Alberto Barradas <abcsds@gmail.com>
2020-05-23 09:22:01 +0200

    Update cluster analysis: Update naming convention Update to Emotion-to-Emotion correlation clustering

--- exploration/04_cluster.py
+++ exploration/04_cluster.py
@@ -1,10 +1,14 @@
+import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+
 
 sns.set_style('darkgrid')
 sns.set_palette('muted')
+enc = OneHotEncoder(sparse=False)
 
 dss = [
        "data/CrowdFlower/FastText/embeddings_unsupervised.csv",
@@ -24,19 +28,34 @@ dss = [
        "data/Friends/BERT/embeddings.csv",
        ]
 for i, ds in enumerate(dss):
-    print(f"Working on {ds}")
+    data, model = os.path.split(ds)[0].split("/")[1:3]
+    print(f"Working on {data}/{model}")
     df = pd.read_csv(ds)
+    df = df[~df["emotion"].isin(["neutral", "non-neutral", "empty"])]
     X = df.drop("emotion", axis=1).to_numpy()
     Y = df["emotion"].to_numpy()
     assert X.shape[0] == Y.shape[0]
     ind = list(np.unique(Y))
-    for emotion in ind:
-        x = X[Y == emotion]
-        try:
-            p = sns.clustermap(np.corrcoef(x), figsize=(16, 16))
-            p.fig.suptitle(f"Clustermap of {ds} {emotion}", fontsize=24)
-            p.savefig(f"./img/cls/clustermap_{i}_{emotion}.png")
-            del p
-            plt.close()
-        except FloatingPointError:
-            continue
+    # for emotion in ind:
+    #     x = X[Y == emotion]
+    #     try:
+    #         p = sns.clustermap(np.corrcoef(x), figsize=(16, 16))
+    #         p.fig.suptitle(f"Clustermap of {ds} {emotion}", fontsize=24)
+    #         p.savefig(f"./img/cls/cm_{data}_{model}_{emotion}.png")
+    #         del p
+    #         plt.close()
+    #     except FloatingPointError:
+    #         continue
+
+    # Emotion to emotion correlation
+    enc.fit(np.unique(Y).reshape(-1,1))
+    mat = enc.transform(Y.reshape(-1,1))
+    cor = np.corrcoef(mat.transpose())
+    p = sns.clustermap(cor, figsize=(16, 16))
+    t = [int(tick_label.get_text()) for tick_label in p.ax_heatmap.axes.get_yticklabels()]
+    sorted_e = [x for _,x in sorted(zip(t,ind))]
+    p.ax_heatmap.axes.set_yticklabels(sorted_e,  rotation=0)
+    p.fig.suptitle(f"Clustermap of {ds} E-E", fontsize=24)
+    p.savefig(f"./img/cls/cmE_{data}_{model}.png")
+    del p
+    plt.close()
